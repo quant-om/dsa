ae88f2694c6b6ec5d70dec9fd8083a4121cfcba9
vijai.sm <vijai.sm@gmail.com>
2024-09-11 06:39:11 +0000

    added array data structure

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -11,6 +11,8 @@
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
+    pkgs.python3
+    pkgs.apt
   ];
 
   # Sets environment variables in the workspace
---
+++ src/linear/static/1_array.py
@@ -0,0 +1,8 @@
+import array as arr
+
+x = arr.array('i', [1, 2, 3])
+print(x)
+print(type(x))
+
+x.append(4)
+print(x)
